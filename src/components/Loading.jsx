739320c895b898ce5c6edb57b277ecced3448052
zaidjamal-op <iamzaidbinarshad@gmail.com>
2023-01-04 15:14:58 +0530

    adding loading screen

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loading() {
+  return (
+      <div className='w-full min-h-screen flex items-center justify-center'>
+          <h1 className="text-white text-2xl font-extrabold">Loading...</h1>
+        </div>
+  )
+}
+
+export default Loading
