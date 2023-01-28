ad88005ccf19e7e1c646509a0147d40be25314f1
Awesome UFO Dude <luisfelipejimenezroldan@gmail.com>
2023-01-28 10:48:26 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
+    <div className='flex items-center justify-center lg:justify-end py-3 px-8'>
+          <p className='text-project_white'></p>
+    </div>
   )
 }
 
