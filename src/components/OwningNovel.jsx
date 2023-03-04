c49406d90fd6229e4eb23c9684412926863c13d7
zaidjamal-op <iamzaidbinarshad@gmail.com>
2023-03-04 19:40:18 +0530

    fixing buy card

--- src/components/OwningNovel.jsx
+++ src/components/OwningNovel.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function OwningNovel() {
   return (
-    <div className="lg:max-w-4xl w-[90%] mx-auto my-">
+    <div className="lg:max-w-4xl w-[90%] mx-auto my-14">
       <div className="relative group">
         <div className="absolute -inset-1 bg-gradient-to-r from-project_orange to-project_orange/30 rounded-lg blur opacity-40 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
         <div className="relative px-7 py-6 bg-project_black_shade ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
-          <div className="space-y-3">
+          <div className="space-y-3 lg:max-w-2xl w-full mx-auto ">
             <p className="text-project_white/50 leading-6 text-center">
              If you are interested in owning the complete rights of these novels and game design, click below
             </p>
